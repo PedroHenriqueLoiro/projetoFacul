fa874fd5f4e2d2ac88887ed6e7db573ac26daed1
pedro henrique <pedroguedesufrn@outlook.com>
2021-01-21 20:56:44 -0300

    terceiro commit

--- projeto.c
+++ projeto.c
@@ -31,7 +31,7 @@ void menuSobre(void) {
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                         ///\n");
-    printf("///            = = = = = Banco = = = = =                ///\n");
+    printf("///            = = = = = Banco 02 = = = = =                ///\n");
     printf("///                                                                         ///\n");
     printf("///  Programa exemplo utilizado na disciplina DCT1106 - Programação, para   ///\n");
     printf("///  fins didáticos de ilustração. O programa consiste em um pragama que    ///\n");
